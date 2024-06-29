5be2d3d132ea965db1364562de96a0352430fab4
Paavo Karppinen <paavo_karppinen@outlook.com>
2024-06-29 14:26:05 +0300

    Add movie fetching from api after login

--- CineregWPF/MainPage.xaml.cs
+++ CineregWPF/MainPage.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using CineregWPF.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace CineregWPF
@@ -8,9 +12,41 @@ namespace CineregWPF
     /// </summary>
     public partial class MainPage : Page
     {
+        private TokenResponse _tokenResponse;
+
+
+        JsonSerializerOptions Options = new()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
         public MainPage(TokenResponse tokenResponse)
         {
             InitializeComponent();
+            _tokenResponse = tokenResponse;
+            Loaded += OnLoaded;
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            HttpClient httpClient = new();
+            MainWindow mainWindow = (MainWindow)Window.GetWindow(this);
+
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_tokenResponse.AccessToken}");
+            var result = await httpClient.GetAsync("https://cinereg-pk.azurewebsites.net/api/Movies");
+
+            if (result.IsSuccessStatusCode)
+            {
+                var movies = await result.Content.ReadFromJsonAsync<List<Movie>>();
+
+                movieEntries.ItemsSource = movies;
+            }
+        }
+
+        private async void AddMovieButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ CineregWPF/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineregWPF.Models
+{
+    public class Movie
+    {
+        public string? Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int ReleaseYear { get; set; } = DateTime.Now.Year;
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Director { get; set; }
+        public string? UserId { get; set; }
+        [Required]
+        public int WatchedYear { get; set; } = DateTime.Now.Year;
+        [Required]
+        public string ViewingForm { get; set; }
+        [Required]
+        public string Review { get; set; }
+    }
+}
